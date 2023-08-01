c0c04ab27c6d2f1804903e0639e6b2a922583de7
Grichnak <marekdimitri@gmail.com>
2023-08-01 10:55:57 +0200

    404 added

--- src/pages/notFound/pageNotFound.jsx
+++ src/pages/notFound/pageNotFound.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import './pageNotFound.scss'
+import { Link } from 'react-router-dom'
 
-
-export const PageNotFound = () => {
+function PageNotFound() {
     return (
-        <div>
-            404
+        <div className='error'>
+            <span>404</span>
+            <h1>Oups! La page que vous demandez n`existe pas.</h1>
+            <Link className="errorLink" to="/">Retourner sur la page d`accueil</Link>
         </div>
     )
 }
+ 
+export default PageNotFound
